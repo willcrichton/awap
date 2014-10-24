b617b063a1904db56c31e857662f82fc316f9afc
Dillon Lareau <flybye22@gmail.com>
2014-10-24 17:40:47 -0400

    buttons now have test features

--- server/www/page_admin.js
+++ server/www/page_admin.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
     var ws = io.connect(location.protocol+'//'+location.hostname+':8080');
+
+    function kick (teamId) {
+        console.log("test");
+        //ws.emit('adminKickRequest', teamId);
+    }
+
+    function start () {
+        ws.emit('adminStartRequest', teamId);
+    }
+
     ws.on('returnInfo', function(data) {
         var $list = $('#games');
         $list.html("<tr>\
@@ -12,7 +22,8 @@ $(document).ready(function() {
                 var title = game.join(", ");
                 $list.append('<tr style="background-color: red">\
                                 <td>' + title + '</td>\
-                                <td style="text-align:center"> <button type="button">Click Me!</button></td>\
+                                <td style="text-align:center">
+                                </td>\
                                 <td>other field</td>\
                               </tr>');
             }
@@ -22,7 +33,9 @@ $(document).ready(function() {
                 var title = game.teams.join(", ");
                 $list.append('<tr style="background-color: orange">\
                                 <td><a href="game.html#' + game.id + '">' + title + '</a></td>\
-                                <td style="text-align:center"> <button type="button">Click Me!</button></td>\
+                                <td style="text-align:center">
+                                    <button type="button" onclick="start(' + team + ')">Click Me!</button>
+                                </td>\
                                 <td>other field</td>\
                               </tr>');
             }
@@ -32,7 +45,8 @@ $(document).ready(function() {
                 var title = game.teams.join(", ");
                 $list.append('<tr style="background-color: green">\
                                 <td><a href="game.html#' + game.id + '">' + title + '</a></td>\
-                                <td style="text-align:center"> <button type="button">Click Me!</button></td>\
+                                <td style="text-align:center">
+                                </td>\
                                 <td>other field</td>\
                               </tr>');
             }
@@ -46,13 +60,17 @@ $(document).ready(function() {
             if(data.connectedTeams.indexOf(team) >= 0){
                 $list.append('<tr style="background-color: green">\
                                   <td>' + team + '</td>\
-                                  <td style="text-align:center"> <button type="button">Click Me!</button></td>\
+                                  <td style="text-align:center"> 
+                                    <button type="button" onclick="kick(' + team + ')">Click Me!</button>
+                                  </td>\
                               </tr>');
             }
             else{
                 $list.append('<tr style="background-color: red">\
                                   <td>' + team + '</td>\
-                                  <td style="text-align:center"> <button type="button">Click Me!</button></td>\
+                                  <td style="text-align:center">
+                                    <button type="button" onclick="kick(' + team + ')">Click Me!</button>
+                                  </td>\
                               </tr>');
             }
         }
