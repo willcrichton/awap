640782955950ef5019d5be314353c115edda10b1
Will Crichton <willcrichton@cmu.edu>
2015-06-07 16:06:26 -0400

    Cleaned out old code

--- Makefile
+++
@@ -1,4 +0,0 @@
-upload:
-	tar -cf client.tar client
-	scp client.tar wcrichto@acmalgo.com:awap/server/www/
-	rm client.tar
--- client/java/src/awap/Logger.java
+++
@@ -1,7 +0,0 @@
-package awap;
-
-public class Logger {
-	public static void log(String message) {
-		System.out.println("DEBUG " + message);
-	}
-}
