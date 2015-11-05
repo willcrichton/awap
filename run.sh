b98ea82834214437a9ac686ffb176e17dfda5fc6
Will Crichton <willcrichton@cmu.edu>
2015-11-04 19:17:34 -0500

    Conflicting commit

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 if [[ -n "$VIRTUAL_ENV" ]]; then
     python src/main.py $@
 else
-    echo "You need to activate virtualenv before using the server. Run:"
+    echo "You need to activate virtualenv before using the server. I AM A CONFLICT"
     echo "source venv/bin/activate"
 fi
